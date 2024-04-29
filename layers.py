8aaba2647273699827aecda9eea4dacd6a7064d1
viggoh10 <viggo.haglund10@gmail.com>
2024-04-29 17:21:20 +0200

    Add dropout layer class

--- layers.py
+++ layers.py
@@ -35,3 +35,21 @@ class Dense:
 
 		self.dinputs = np.dot(dvalues, self.weights.T)
 
+
+class Dropout: 
+	def __init__(self, rate):
+		self.rate = 1-rate
+		
+	def forward(self, inputs, training):
+
+		self.inputs = inputs
+		if not training:
+			self.output = inputs.copy()
+			return
+	
+		self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape)/self.rate
+		self.output = inputs*self.binary_mask
+
+	def backward(self, dvalues):
+		self.dinputs = self.binary_mask*dvalues
+
